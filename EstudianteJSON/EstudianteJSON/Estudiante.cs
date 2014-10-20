afc37b4181f35be9d5584cbf1a4e62b3390a048b
osvaldo <oca159@hotmail.es>
2014-10-20 10:14:31 -0500

    Prueba de servicios JSON en una aplicación WCF

---
+++ EstudianteJSON/EstudianteJSON/Estudiante.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EstudianteJSON
+{
+    public class Estudiante
+    {
+        public string Nombre { get; set; }
+        public string Carrera { get; set; }
+        public int Edad { get; set; }
+    }
+}
---
+++ EstudianteJSON/EstudianteJSON/Service1.svc.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace EstudianteJSON
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public Estudiante[] GetEstudiantes()
+        {
+            Estudiante e1 = new Estudiante();
+            e1.Nombre = "Osvaldo";
+            e1.Carrera = "Informatica";
+            e1.Edad = 21;
+
+            Estudiante e2 = new Estudiante();
+            e2.Nombre = "Cristian";
+            e2.Carrera = "Medicina";
+            e2.Edad = 22;
+
+            List<Estudiante> estudiantes = new List<Estudiante>();
+            estudiantes.Add(e1);
+            estudiantes.Add(e2);
+            return estudiantes.ToArray();
+        }
+    }
+}
